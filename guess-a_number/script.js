1553b7a0160aa70183f1a5d1623fb899022b9d98
SRIHARIARUMURU <128991102+SRIHARIARUMURU@users.noreply.github.com>
2025-09-02 11:07:05 +0530

    Add files via upload

--- guess-a_number/script.js
+++ guess-a_number/script.js
@@ -1,20 +1,23 @@
-let random=Math.ceil(Math.random()*100)
-let inp=document.getElementById("inp")
-let res=document.getElementById("res")
+let random = Math.ceil(Math.random() * 100)
+let inp = document.getElementById("inp")
+let res = document.getElementById("res")
 
 console.log(random)
-
-function guess_button(){
-    if (inp.value == random){
-        res.textContent="Congrats You Won!"
-        res.style.color="green"
+count = 0
+function guess_button() {
+    if (inp.value == random) {
+        count += 1
+        res.textContent = "Congrats You Won! in " + count + " chances"
+        res.style.color = "green"
     }
-    else if (inp.value<random){
-        res.textContent="Oops Your Guessing is Too Low. try Again!"
-        res.style.color="red"
+    else if (inp.value < random) {
+        res.textContent = "Oops Your Guessing is Too Low. try Again!"
+        res.style.color = "red"
+        count += 1
     }
-    else{
-        res.textContent="Oops Your Guessing is Too High. try Again!"
-        res.style.color="red"
+    else {
+        res.textContent = "Oops Your Guessing is Too High. try Again!"
+        res.style.color = "red"
+        count += 1
     }
 }
